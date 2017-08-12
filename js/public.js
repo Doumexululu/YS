43a2e343798a88b6d39e4764fed979c66df906ab
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2017-08-12 12:23:44 +0800

    update

--- js/public.js
+++ js/public.js
@@ -6,6 +6,11 @@ $(function(){
         $(this).children(".secondary-menu").toggleClass("displaynone");
         $(this).siblings(".firstary-font").children(".secondary-menu").addClass("displaynone")
     });
+    $(".species-topic .tab-item").click(function(){
+        var _index = $(this).index(".tab-item");
+        // console.log(_index);
+        $(this).parents(".tab").children(".select-tab").children(".tab-cont").eq(_index).css("display","block").siblings().css("display","none");
+    });
     $(".species-topic .tab-item").eq(0).click(function(){
         $(this).siblings(".blue-line").animate({"left":"20px"});
         // $(tabcont:not(eq(_index))).css("display","none");
@@ -13,10 +18,5 @@ $(function(){
     $(".species-topic .tab-item").eq(1).click(function(){
         $(this).siblings(".blue-line").animate({"left":"212px"});
     });
-    $(".species-topic .tab-item").click(function(){
-        var _index = $(this).index(".tab-item");
-        // console.log(_index);
-        // var tabcont = $(this).parents(".tab").children(".tab-cont");
-        $(this).parents(".tab").children(".select-tab .tab-cont").eq(_index).css("display","block");
-    });
+
 })
